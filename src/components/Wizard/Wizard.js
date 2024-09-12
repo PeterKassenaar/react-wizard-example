9f687cc98f3435db0460e289b8b72719658375be
PeterKassenaar <info@kassenaar.com>
2024-09-12 13:29:38 +0200

    Added comments

--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -4,18 +4,26 @@ import Step2 from "../Step2/Step2";
 import Step3 from "../Step3/Step3";
 
 const Wizard = () => {
+    // The steps in our wizard. Initial step is Step 1.
     const [step, setStep] = React.useState(1);
+
+    //******************
+
+    // Go to next step in Wizard (if available)
     const nextStep = () => {
         if (step < 3) {
             setStep(step + 1);
         }
     }
+
+    // Go to previous step in Wizard (if available)
     const prevStep = () => {
         if (step > 1) {
             setStep(step - 1);
         }
     }
 
+    // Render the current step in the Wizard component
     const renderStep = () => {
         switch (step) {
             case 1: {
@@ -38,6 +46,8 @@ const Wizard = () => {
         <div>
             <h1>Wizard component</h1>
             {renderStep()}
+            {/* A small bar below the Steps, containing the Previous/Next buttons for the wizard. Of course
+             they can be in their own component if you wish to do so. */}
             <div>
                 {step > 1 &&
                     <button className="btn bg-info float-start"
