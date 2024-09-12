9745e34b3dd1c32c3178ace57de31e56f2db017a
PeterKassenaar <info@kassenaar.com>
2024-09-12 14:34:19 +0200

    Updated files for another wizard step-new-approach

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import Wizard from "./Wizard/Wizard";
+import Step1 from "./Step1/Step1";
+import Step2 from "./Step2/Step2";
+import Step3 from "./Step3/Step3";
 
 const App = () => {
     return (
         <div className="container">
-           <Wizard/>
+           {/*  We now have a wizard with a variable number of steps. They
+            are passed to the Wizard component as 'children' */}
+           <Wizard>
+               <Step1 />
+               <Step2 />
+               <Step3 />
+           </Wizard>
         </div>
     );
 }
--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -1,43 +1,31 @@
 import React from 'react';
-import Step1 from "../Step1/Step1";
-import Step2 from "../Step2/Step2";
-import Step3 from "../Step3/Step3";
 
-const Wizard = () => {
-    // The steps in our wizard. Initial step is Step 1.
-    const [step, setStep] = React.useState(1);
+
+// This is another approach, where the children of the wizard component are passed in as props.
+// The result is functionally (more or less) the same as the branches `step-1` and `step-2`, but
+// follows a different design pattern.
+
+const Wizard = ({children}) => {
+    // The steps in our wizard. Initial index is Step 1.
+    const [index, setIndex] = React.useState(0);
+
+    // We split the children that are coming in as props to an array:
+    const steps = React.Children.toArray(children)
+    const currentStep = steps[index];
 
     //******************
 
-    // Go to next step in Wizard (if available)
+    // Go to next index in Wizard (if available)
     const nextStep = () => {
-        if (step < 3) {
-            setStep(step + 1);
+        if (index < steps.length - 1) {
+            setIndex(index + 1);
         }
     }
 
-    // Go to previous step in Wizard (if available)
+    // Go to previous index in Wizard (if available)
     const prevStep = () => {
-        if (step > 1) {
-            setStep(step - 1);
-        }
-    }
-
-    // Render the current step in the Wizard component
-    const renderStep = () => {
-        switch (step) {
-            case 1: {
-                return <Step1/>;
-            }
-            case 2: {
-                return <Step2/>;
-            }
-            case 3: {
-                return <Step3/>;
-            }
-            default: {
-                return <Step1/>;
-            }
+        if (index > 0) {
+            setIndex(index - 1);
         }
     }
 
@@ -45,17 +33,18 @@ const Wizard = () => {
     return (
         <div>
             <h1>Wizard component</h1>
-            {renderStep()}
-            {/* A small bar below the Steps, containing the Previous/Next buttons for the wizard. Of course
-             they can be in their own component if you wish to do so. */}
-            <div>
-                {step > 1 &&
+            <div className="content">
+                {currentStep}
+            </div>
+            <hr/>
+            <div className="alert bg-light">
+                {index > 0 &&
                     <button className="btn bg-info float-start"
                             onClick={prevStep}>Previous</button>
                 }
-                {step < 3 &&
+                {index < (steps.length -1) &&
                     <button className="btn bg-info float-end"
-                        onClick={nextStep}>Next</button>
+                            onClick={nextStep}>Next</button>
                 }
             </div>
         </div>
